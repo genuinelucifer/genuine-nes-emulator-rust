54b4eef063876b9227781e913d518078cad09b00
Ravi Kumar <rdahiya0213@gmail.com>
2019-08-29 14:22:22 +0900

    list test roms

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # genuine-nes-emulator-rust
 Emulator for the NES system in Rust
+
+
+#### Build
+
+    Use `cargo build --features "debug"` to build with test roms and then 
+    run as `cargo run --features "debug"`.
+   
+    For main release use `"release"` instead of `"debug"`
--- src/nes_loader/mod.rs
+++ src/nes_loader/mod.rs
@@ -1,4 +1,16 @@
+extern crate walkdir;
 
-pub fn list_test_roms() {
-    //TODO:: add test roms and then list them
+use walkdir::WalkDir;
+
+// Only for `.nes` file
+pub fn list_test_roms(path: &str) -> Vec<walkdir::DirEntry>{
+    let mut roms = WalkDir::new(path)
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_name().to_string_lossy().ends_with(".nes"))
+        .collect::<Vec<walkdir::DirEntry>>();
+
+    roms.sort_by(|x, y| String::from(x.path().to_string_lossy()).cmp(&String::from(y.path().to_string_lossy())));
+    roms
 }
